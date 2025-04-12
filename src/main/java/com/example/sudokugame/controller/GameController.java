547dc5411e4a6baeddc350ceba4457fda07a65bc
Isabela Bermudez <isabela.bermudez@correounivalle.edu.co>
2025-04-11 21:01:51 -0500

    The initialize() and showBoard() functions in GameController have been modified

--- src/main/java/com/example/sudokugame/controller/GameController.java
+++ src/main/java/com/example/sudokugame/controller/GameController.java
@@ -3,6 +3,7 @@ package com.example.sudokugame.controller;
 import com.example.sudokugame.model.SudokuBoard;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
@@ -12,15 +13,13 @@ public class GameController {
 
     @FXML
     private GridPane sudokuPanel;
-
     private SudokuBoard sudokuBoard;
-
     private final int SIZE = 6;
 
     @FXML
     public void initialize() {
         sudokuBoard = new SudokuBoard();
-        sudokuArray = sudokuBoard.getSudokuArray();
+        sudokuBoard.printBoard();
         showBoard();
     }
 
@@ -30,11 +29,15 @@ public class GameController {
         for (int row = 0; row < SIZE; row++) {
             for (int col = 0; col < SIZE; col++) {
                 TextField textField = new TextField();
+                textField.setAlignment(Pos.CENTER);
                 textField.setPrefSize(60, 60);
+                int number = sudokuBoard.getBoard().get(row).get(col);
 
-                if (!sudokuArray.get(index).equals(".")) {
-                    textField.setText(sudokuArray.get(index));
+                if (number > 0) {
+                    textField.setText(String.valueOf(number));
                     textField.setDisable(true);
+                } else {
+                    textField.setText("");
                 }
 
                 sudokuPanel.add(textField, col, row);
