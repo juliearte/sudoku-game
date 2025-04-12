2fd938f467ef2f1c06c38dec491bba0d20c9d602
Isabela Bermudez <isabela.bermudez@correounivalle.edu.co>
2025-04-11 19:21:56 -0500

    Created fillBlocks() and isValid() functions

--- src/main/java/com/example/sudokugame/model/SudokuBoard.java
+++ src/main/java/com/example/sudokugame/model/SudokuBoard.java
@@ -1,6 +1,7 @@
 package com.example.sudokugame.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -44,13 +45,75 @@ public class SudokuBoard {
 
 
     //Metodo para llenar los bloques del tablero con dos numeros aleatorios
-    private boolean fillBlocks( int blockIndex) {
-        sudokuArray.clear();
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                sudokuArray.add(board[i][j] == 0 ? "." : String.valueOf(board[i][j]));
+    private boolean fillBlocks(int blockIndex) {
+        if(blockIndex == TOTAL_BLOCKS) {
+            return true;
+        }
+
+        int blockRow = blockIndex / TOTAL_BLOCK_ROWS;
+        int blockCol = blockIndex % TOTAL_BLOCK_COLS;
+        int startRow = blockRow * block_rows;
+        int startCol = blockCol * block_cols;
+
+        //obtner las dos primeras celdas del bloque
+        List<int[]> firstTwoCells = new ArrayList<>();
+        outerloop:
+        for(int i = startRow; i < startRow + block_rows; i++) {
+            for(int j = startCol; j < startCol + block_cols; j++) {
+                firstTwoCells.add(new int[]{i, j});
+                if(firstTwoCells.size() == 2){
+                    break outerloop;
+                }
             }
         }
+
+        //generar una lista de numeros entre el 1 hasta el 6, ordenados aleatoriamente
+        List<Integer> numbers = new ArrayList<>();
+        for(int i = 1; i <= SIZE; i++) {
+            numbers.add(i);
+        }
+        Collections.shuffle(numbers,random);
+
+        //Intentar cololar cada par de numeros diferentes en las dos celdas
+        for(int i = 0; i < numbers.size(); i++) {
+            for (int j = i + 1; j < numbers.size(); j++) {
+                int num1 = numbers.get(i);
+                int num2 = numbers.get(j);
+
+                int[] cell1 = firstTwoCells.get(0);
+                int[] cell2 = firstTwoCells.get(1);
+
+                if(isValid(cell1[0],cell1[1], num1) &&
+                    isValid(cell2[0], cell2[1], num2)) {
+
+                    board.get(cell1[0]).set(cell1[1], num1);
+                    board.get(cell2[0]).set(cell2[1], num2);
+
+                    if (fillBlocks(blockIndex + 1)){
+                        return true;
+                    }
+
+                    //backtrack
+                    board.get(cell1[0]).set(cell1[1], 0);
+                    board.get(cell2[0]).set(cell2[1], 0);
+                }
+            }
+        }
+
+        return false; //no se pudo llenar el bloque
+    }
+    public boolean isValid(int row, int col, int candidate) {
+        for(int j = 0; j < SIZE; j++) {
+            if(board.get(row).get(j) == candidate) {
+                return false;
+            }
+        }
+        for(int i = 0; i < SIZE; i++) {
+            if(board.get(i).get(col) == candidate) {
+                return false;
+            }
+        }
+        return true;
     }
 
     //estructura de dato
