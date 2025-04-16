16c0253c6fe14e3a57a3250e3bcd1af734375956
Julieta Arteta <arteta.aura@correounivalle.edu.co>
2025-04-16 17:05:12 -0500

    feat: improve game board and UI elements
    - Prevent number repetition on the board.
    - Clear unused numbers from the board.
    - Update configureTextField method to validate numbers.
    - Add icon to game windows.
    - Visually display remaining lives on the screen.

--- src/main/java/com/example/sudokugame/view/HomeStage.java
+++ src/main/java/com/example/sudokugame/view/HomeStage.java
@@ -3,6 +3,7 @@ package com.example.sudokugame.view;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,7 @@ public class HomeStage extends Stage {
         FXMLLoader fmxlLoad = new FXMLLoader(getClass().getResource("/com/example/sudokugame/game-view.fxml"));
         Parent root = fmxlLoad.load();
         Scene scene = new Scene(root, 600, 400);
+        this.getIcons().add(new Image(getClass().getResourceAsStream("/com/example/sudokugame/icon.png")));
         setTitle("Sudoku Game");
         setScene(scene);
         setResizable(false);
